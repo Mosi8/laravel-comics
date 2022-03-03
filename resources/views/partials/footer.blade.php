83850df2db25a0795288d1f8977ca5d078b41f05
Mosi8 <mauriziomosi8@gmail.com>
2022-03-03 17:56:45 +0100

    mancano dettagli  sopra header

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,4 +1,129 @@
 <footer>
+    <div class="footer-iniziale">
+        <div class="container container-tessere">
+            <div class="tessera">
+                <img src="{{asset('images/buy-comics-digital-comics.png')}}">
+                <span class="titoli-icone">DIGITAL COMICS</span>
+            </div>
+            <div class="tessera">
+                <img src="{{asset('images/buy-comics-merchandise.png')}}">
+                <span class="titoli-icone">DC MERCHANDISE</span>
+            </div>
+            <div class="tessera">
+                <img src="{{asset('images/buy-comics-subscriptions.png')}}">
+                <span class="titoli-icone">SUBSCRIPTION</span>
+            </div>
+            <div class="tessera">
+                <img src="{{asset('images/buy-comics-shop-locator.png')}}">
+                <span class="titoli-icone">COMIC SHOP LOCATOR</span>
+            </div>
+            <div class="tessera">
+                <img src="{{asset('images/buy-dc-power-visa.svg')}}">
+                <span class="titoli-icone">DC POWER VISA</span>
+            </div>
+        </div>
+    </div>
+    <div class="footer-centrale">
+        <div class="container">
+            <div class="primaColonna">
+              <h4>DC COMICS</h4>
+              <ul>
+                <li>
+                  <a href="#">Characters</a>
+                </li>
+                <li>
+                  <a href="#">Comics</a>
+                </li>
+                <li>
+                  <a href="#">Movies</a>
+                </li>
+                <li>
+                  <a href="#">TV</a>
+                </li>
+                <li>
+                  <a href="#">Games</a>
+                </li>
+                <li>
+                  <a href="#">Videos</a>
+                </li>
+                <li>
+                  <a href="#">New</a>
+                </li>
+              </ul>
+              <h4>SHOP</h4>
+              <ul>
+                <li>
+                  <a href="#">Shop DC</a>
+                </li>
+                <li>  
+                  <a href="#">Shop DC Collectibles</a>
+                </li>
+              </ul>
+            </div>
+            <div class="secondaColonna">
+              <h4>DC</h4>
+              <ul>
+                <li>
+                  <a href="#">Terms of Use</a>
+                </li>
+                <li>
+                  <a href="#">Privacy policy(New)</a>
+                  </li>
+                  <li>
+                  <a href="#">Ad Choices</a>
+                  </li>
+                  <li>
+                  <a href="#">Advertising</a>
+                  </li>
+                  <li>
+                  <a href="#">Jobs</a>
+                  </li>
+                  <li>
+                  <a href="#">Subscriptions</a>
+                  </li>
+                  <li>
+                  <a href="#">Talent Workshops</a>
+                  </li>
+                  <li>
+                  <a href="#">CPSC Certificates</a>
+                  </li>
+                  <li>
+                  <a href="#">Ratings</a>
+                  </li>
+                  <li>
+                  <a href="#">Shop Help</a>
+                  </li>
+                  <li>
+                  <a href="#">Contact Us</a>
+                  </li>
+                  <li>
+                </li>
+              </ul>
+            </div>
+            <div class="terzaColonna">
+              <h4>DC COMICS</h4>
+              <ul>
+                <li>
+                  <a href="#">DC</a>
+                </li>
+                <li>
+                  <a href="#">MAD Magazine</a>
+                </li>
+                <li>
+                  <a href="#">DC Kids</a>
+                </li>
+                <li>
+                  <a href="#">DC Universe</a>
+                </li>
+                <li>
+                  <a href="#">DC Power Visa</a>
+                </li>
+              </ul>
+            </div>
+            
+            <img src="{{asset('images/dc-logo-bg.png')}}">
+          </div>
+    </div>
     <div class="footer-finale">
         <div class="container">
             <div class="sign">
